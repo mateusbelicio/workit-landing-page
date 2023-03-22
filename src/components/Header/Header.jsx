663ab37d59cdefbc3add16a3122cd1f3ce9190bf
Mateus Belicio <mateusbelicio@hotmail.com>
2023-03-22 09:27:31 -0300

    Feat: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,16 @@ import { HeaderStyled } from './HeaderStyled';
 import logo from '../../assets/logo-light.svg';
 
 import React from 'react';
+import Button from '../Button/Button';
 
 const Header = () => {
   return (
     <HeaderStyled className='header'>
-      <div className='header__container container'>
-        <img src={logo} alt='logo' />
-        <button className='button'>Apply for access</button>
+      <div className='header__container container flex'>
+        <img src={logo} alt='Workit logo' className='header__logo' />
+        <Button type='a' href='#'>
+          Apply for access
+        </Button>
       </div>
     </HeaderStyled>
   );
--- src/components/Header/HeaderStyled.jsx
+++ src/components/Header/HeaderStyled.jsx
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const HeaderStyled = styled.header``;
+export const HeaderStyled = styled.header`
+  background-color: var(--clr-primary-400);
+  color: var(--clr-neutral-100);
+  padding-block: 2rem;
+
+  .header {
+    &__container {
+      justify-content: space-between;
+    }
+
+    &__logo {
+      height: 1.5rem;
+      width: 6rem;
+    }
+  }
+`;
